f7eb24e8ac4d55b56f9958acdb947cd91f85bf08
antc <write@andreasvirkus.me>
2018-02-12 19:38:55 +0200

    Add accommodation content

--- src/components/AreaMap.vue
+++ src/components/AreaMap.vue
@@ -15,6 +15,34 @@
     name: 'area-map'
   }
 </script>
+<script>
+  // function initMap() {
+
+  //   // Create a new StyledMapType object, passing it an array of styles,
+  //   // and the name to be displayed on the map type control.
+  //   var styledMapType = new google.maps.StyledMapType(
+  //       ,
+  //     {name: 'Styled Map'}
+  //   );
+
+  //   // Create a map object, and include the MapTypeId to add
+  //   // to the map type control.
+  //   var map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {lat: 55.647, lng: 37.581},
+  //     zoom: 11,
+  //     mapTypeControlOptions: {
+  //       mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+  //               'styled_map']
+  //     }
+  //   });
+
+  //   //Associate the styled map with the MapTypeId and set it to display.
+  //   map.mapTypes.set('styled_map', styledMapType);
+  //   map.setMapTypeId('styled_map');
+  // }
+</script>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
 
 <style>
   hr {

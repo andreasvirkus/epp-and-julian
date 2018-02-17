e3a4c69d32437090df04415fb8ead8180d57a751
andreas virkus <andreas.virkus@bigbank.ee>
2018-02-17 13:26:11 +0200

    Add NavLink as a global component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import NavLink from './components/NavLink'
 
 Vue.config.productionTip = false
+Vue.component(NavLink)
 
 /* eslint-disable no-new */
 new Vue({
